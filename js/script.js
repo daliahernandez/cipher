42cf79e0863dba66462e7d12cdb63e11bf947caf
dalia hernandez <daliahdzr98@gmail.com>
2018-11-12 16:00:23 -0800

    add a prompt so the user can enter a sentance then store it to a variable and print it with an alert

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var userAnswer = prompt("Enter a sentance");
+alert(userAnswer);
+
+var newSentance = function(word){
+
+  return word;
+}
+
+Next, create a function that will take the first and last letter of the sentence, capitalize them, and return this as a new string.
